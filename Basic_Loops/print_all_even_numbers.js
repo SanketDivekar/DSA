1da4f2803a0fd8e2098caad1aceb67ad9247c165
SanketDivekar <sktdivekar@gmail.com>
2026-03-22 11:31:56 +0530

    Modified the input array.

--- Basic_Loops/print_all_even_numbers.js
+++ Basic_Loops/print_all_even_numbers.js
@@ -1,12 +1,12 @@
 function printEvenNumbers(arr) {
-    if (arr.length < 1) {
-        return -1;
+  if (arr.length < 1) {
+    return -1;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      console.log(arr[i]);
     }
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] % 2 == 0) {
-            console.log(arr[i]);
-        }
-    }
+  }
 }
 
-printEvenNumbers([2]);
+printEvenNumbers([2, 56, 78, 11, 33, 6, 4]);
