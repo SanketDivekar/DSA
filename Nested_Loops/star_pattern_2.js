8540e644c983c4e3dba3af4485531a06b12bd469
SanketDivekar <sktdivekar@gmail.com>
2026-03-25 22:27:35 +0530

    Modified the inner loop condition.

--- Nested_Loops/star_pattern_2.js
+++ Nested_Loops/star_pattern_2.js
@@ -2,9 +2,9 @@ function printStarPattern(n) {
   if (n < 0) {
     console.log("-1");
   }
-  for (let i = 0; i <= n; i++) {
+  for (let i = 0; i < n; i++) {
     let row = "";
-    for (let j = 0; j < i; j++) {
+    for (let j = 0; j < i + 1; j++) {
       row = row + "* ";
     }
     console.log(row);
