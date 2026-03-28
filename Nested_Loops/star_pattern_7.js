fa68b1e517c0e39d9b233b7e3a3cbb16ae57c6f4
SanketDivekar <sktdivekar@gmail.com>
2026-03-28 17:36:08 +0530

    Code to print star pattern.

---
+++ Nested_Loops/star_pattern_7.js
@@ -0,0 +1,28 @@
+function printStarPattern(n) {
+  if (n < 0) {
+    console.log("-1");
+  }
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+      if (j < n - (i + 1)) {
+        row = row + " ";
+      } else {
+        row = row + "*";
+      }
+    }
+    console.log(row);
+  }
+}
+
+printStarPattern(5);
+
+// above code will print below pattern
+
+// n = 5
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
