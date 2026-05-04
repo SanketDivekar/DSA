52fc63e7f8a67e02b1bb26fef25de4e74bbc3654
SanketDivekar <sktdivekar@gmail.com>
2026-05-04 22:45:44 +0530

    Added some extra function for adding element to list and updated some comments.

--- LinkedList/design_linklist.js
+++ LinkedList/design_linklist.js
@@ -1,6 +1,6 @@
 // Code to create a link list and traverse a linked list
 
-// Creating a node
+// Blueprint to create a node
 class CreateNode {
   constructor(val) {
     this.val = val;
@@ -8,20 +8,77 @@ class CreateNode {
   }
 }
 
-// Create a empty list
+// Blueprint to create a linklist
 class MyLinkedList {
   constructor() {
     this.head = null;
+    this.size = 0;
   }
 }
 
-// create a instance of MyLinkedList
+let idx;
+
+// create a instance of MyLinkedList below line will create an empty list
 const list = new MyLinkedList();
+
+// function to add element at head of the list
+function addElementAtHead(val) {
+  // create a new node
+  let newNode = new CreateNode(val);
+
+  // point new node to head of the list
+  newNode.next = list.head;
+
+  // point head to the new node
+  list.head = newNode;
+  list.size += 1;
+}
+
+// function to add element at the tail of the list
+function addElementAtTail(val) {
+  // create a new node
+  let newNode = new CreateNode(val);
+
+  //check if list is empty or not, if list is empty point head to newNode else traverse through list using reference variable, when it reaches the last node point reference variable.next = newNode
+  if (list.head == null) {
+    list.head = newNode;
+  } else {
+    let curr = list.head;
+    while (curr.next != null) {
+      curr = curr.next;
+    }
+    curr.next = newNode;
+  }
+  list.size += 1;
+}
+
+// function to add element before index position
+function addElementAtIndex(index, val) {
+  let newNode = new CreateNode(val);
+  idx = index;
+  if (index == 0) {
+    addElementAtHead(val);
+    return;
+  } else if (index == list.size) {
+    addElementAtTail(val);
+    return;
+  } else {
+    let curr = list.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    newNode.next = curr.next;
+    curr.next = newNode;
+  }
+  list.size += 1;
+}
 
 // create nodes for list
 const nodeOne = new CreateNode(10);
 const nodeTwo = new CreateNode(20);
 const nodeThree = new CreateNode(30);
+
+list.size += 3;
 
 // Reference each node to next node
 nodeOne.next = nodeTwo;
@@ -33,7 +90,54 @@ list.head = nodeOne;
 
 // Traversing the list
 let current = list.head;
-console.log("Linked list contains below values: ");
+console.log(
+  "Linked list contains below values: and size of list is: ",
+  list.size,
+);
+
+while (current) {
+  console.log(current.val);
+  current = current.next;
+}
+
+addElementAtHead(11);
+
+// Traversing the list
+current = list.head;
+console.log(
+  "\nLinked list contains below values after adding element at head position: and size of list is: ",
+  list.size,
+);
+
+while (current) {
+  console.log(current.val);
+  current = current.next;
+}
+
+addElementAtTail(22);
+
+// Traversing the list
+current = list.head;
+console.log(
+  "\nLinked list contains below values after adding element at tail position: and size of list is: ",
+  list.size,
+);
+
+while (current) {
+  console.log(current.val);
+  current = current.next;
+}
+
+addElementAtIndex(3, 33);
+
+// Traversing the list
+current = list.head;
+console.log(
+  "\nLinked list contains below values after adding element before index",
+  idx,
+  "and size of list is: ",
+  list.size,
+);
 
 while (current) {
   console.log(current.val);
